8e0e35c9a728f5a22987263f77ed2608fabeb26c
yanyee <houdinisparks@gmail.com>
2015-11-30 15:42:48 +0800

    briefly planned out the structure of the app.

--- app/src/main/java/com/example/yanyee/iotpet/HomeFragment.java
+++ app/src/main/java/com/example/yanyee/iotpet/HomeFragment.java
@@ -58,7 +58,13 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/yanyee/iotpet/SummaryFragment.java
+++ app/src/main/java/com/example/yanyee/iotpet/SummaryFragment.java
@@ -33,6 +33,7 @@ public class SummaryFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
+     *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment SummaryFragment.
