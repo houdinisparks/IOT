c0f19cd1efb687e3934fc30912c3e2eec900e084
claudiaaw <claudiaaw95@gmail.com>
2015-12-08 01:05:17 +0800

    added graphs to summary fragment

--- app/src/main/java/com/example/yanyee/iotpet/SummaryFragment.java
+++ app/src/main/java/com/example/yanyee/iotpet/SummaryFragment.java
@@ -8,6 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +31,7 @@ import android.view.ViewGroup;
  * Use the {@link SummaryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SummaryFragment extends Fragment {
+public class SummaryFragment extends Fragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -52,21 +66,76 @@ public class SummaryFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public BarChart chart;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false);
+       View view = inflater.inflate(R.layout.fragment_summary, container, false);
+        //return inflater.inflate(R.layout.fragment_summary, container, false);
+        chart = (BarChart) view.findViewById(R.id.chart);
+        BarData data = new BarData(getXAxisValues(), getDataSet());
+        chart.setData(data);
+        chart.setDescription("Amount of water consumed(ml)");
+        chart.animateXY(2000, 2000);
+        chart.invalidate();
+        return view;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_summary);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    private ArrayList<BarDataSet> getDataSet() {
+        ArrayList<BarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(875.000f, 0); // MON
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(1020.000f, 1); // TUE
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(1200.000f, 2); // WED
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(1120.000f, 3); // THURS
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(900.000f, 4); // FRI
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(875.000f, 5); // SAT
+        valueSet1.add(v1e6);
+        BarEntry v1e7 = new BarEntry(980.000f, 5); // SUN
+        valueSet1.add(v1e7);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Water Consumed");
+        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet1);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("MON");
+        xAxis.add("TUE");
+        xAxis.add("WED");
+        xAxis.add("THURS");
+        xAxis.add("FRI");
+        xAxis.add("SAT");
+        xAxis.add("SUN");
+        return xAxis;
+    }
+
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
